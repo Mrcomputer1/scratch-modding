9999cf265514db41bad52f7528eea8fdaa2ecaea
Mrcomputer1 <mrcomputer1email@gmail.com>
2023-08-11 23:10:22 +1000

    Fix <ScratchBlocks> with <BrowserOnly>

--- src/components/ScratchBlocks/index.js
+++ src/components/ScratchBlocks/index.js
@@ -1,33 +1,37 @@
 import clsx from 'clsx';
-import React, { useRef, useEffect, useState } from 'react';
-import scratchblocks from 'scratchblocks';
+import React, { useRef, useEffect } from 'react'
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 import styles from './scratchblocks.module.css';
 
 export default function ScratchBlocks({
     children, style="scratch3", scale=-1, inline=false, languages=["en"]
 }){
+    return <BrowserOnly>
+        {() => {
+            const scratchblocks = require("scratchblocks").default;
 
-    const codeRef = useRef(null);
+            const codeRef = useRef(null);
 
-    useEffect(() => {
-        if(scale === -1){
-            if(style === "scratch2"){
-                scale = 1;
-            }else if(style.startsWith("scratch3")){
-                scale = 0.675;
-            }
-        }
+            useEffect(() => {
+                if(scale === -1){
+                    if(style === "scratch2"){
+                        scale = 1;
+                    }else if(style.startsWith("scratch3")){
+                        scale = 0.675;
+                    }
+                }
 
-        const options = {style, scale, inline, languages}
-        const code = scratchblocks.read(codeRef.current, options);
-        const doc = scratchblocks.parse(code, options);
-        const svg = scratchblocks.render(doc, options);
-        scratchblocks.replace(codeRef.current, svg, doc, options);
-    }, []);
-
-    return <pre ref={codeRef} className={clsx(styles.scratchBlocksContainer)}>
-        {children}
-    </pre>;
-
+                const options = {style, scale, inline, languages}
+                const code = scratchblocks.read(codeRef.current, options);
+                const doc = scratchblocks.parse(code, options);
+                const svg = scratchblocks.render(doc, options);
+                scratchblocks.replace(codeRef.current, svg, doc, options);
+            }, []);
+            
+            return <pre ref={codeRef} className={clsx(styles.scratchBlocksContainer)}>
+                {children}
+            </pre>;
+        }}
+    </BrowserOnly>
 };
